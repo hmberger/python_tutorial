8bb6237702d0efd46ae05ac40fc4ab2bd40530f2
Halle Berger <halleberger@Halles-MacBook-Air.local>
2021-02-20 13:31:19 -0500

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="A sample package",
+    author="Halle Berger",
+    author_email="halle.berger@uconn.edu",
+    packages=["mysci"],
+    install_requires=[])
